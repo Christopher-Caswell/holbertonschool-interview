f7adfc113ebcce73690291eb4ea3ffaafd06afe5
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-11-22 15:48:24 -0600

    Retyped #!

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Minimum number of operations necessary to copy and paste h such that it fulfills n """
+
+def minOperations(n):
+    """ Operate. If n is impossible, return 0 """
+
+    copyAll = 0
+    totalPasted = 0
+    operationIteration = 0
+
+    if n < 2:
+        return 0
+
+    while totalPasted < n:
+        if n % totalPasted == 0:
+            copyAll == totalPasted
+            operationIteration += 1
+    
+        totalPasted += copyAll
+        operationIteration += 1
+
+    if totalPasted == n:
+        return operationIteration
+
+    return 0
