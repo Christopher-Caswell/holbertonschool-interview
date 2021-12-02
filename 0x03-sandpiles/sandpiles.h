918c5ee3d2ead1691d555a7b135874394162ad87
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-12-02 13:19:43 -0600

    include standard library

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -1,6 +1,9 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int stableSand(int grid[3][3]);
 static void print_grid(int grid[3][3]);
