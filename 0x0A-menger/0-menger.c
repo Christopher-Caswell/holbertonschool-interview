80f1a8df0913618423de8b0962ed1dd951bd238c
Christopher-Caswell <christopher.caswell@rocketmail.com>
2022-02-17 09:55:50 -0600

    Changed note types for proper language

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -7,21 +7,21 @@
 */
 void menger(int level)
 {
-// francis is the print-in-place we need to show the sponge
+/* francis is the print-in-place we need to show the sponge */
 char francis = '#';
-// iterators
+/* iterators */
 int x, y, z, calculon = pow(3, level);
-// first loop, defines the vertical/columns
+/* first loop, defines the vertical/columns */
 for (y = 0; y < calculon; y++)
 {
-// second loop, defines the horizontal/rows
+/* second loop, defines the horizontal/rows */
 for (x = 0; x < calculon; x++)
 {
-// the action: redefine francis, iterate, produce the hole
-// print at francis' level all instances contrarywise
+/* the action: redefine francis, iterate, produce the hole */
+/* print at francis' level all instances contrarywise */
 francis = '#';
 for (z = 1; z < calculon; z *= 3)
-// At points x=3 and y=3, the space is the place
+/* At points x=3 and y=3, the space is the place */
 if (y / z % 3 == 1 && x / z % 3 == 1)
 francis = 32;
 putchar(francis);
