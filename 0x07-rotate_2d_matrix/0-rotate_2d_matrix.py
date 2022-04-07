6f6c3bb29194fc0a1cc21f1f0ec6dfab832a0cdb
Christopher-Caswell <christopher.caswell@rocketmail.com>
2022-04-06 22:33:39 -0500

    Pep8

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,6 +4,7 @@ Return nothing. The matrix must be edited in-place.
 Assume the matrix will have 2 dimensions and will not be empty.
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotate matrix 90 degrees clockwise
@@ -13,7 +14,7 @@ def rotate_2d_matrix(matrix):
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
             matrix[x][y] = Rot8[x][y]
-    
+
     return Rot8
 
 """
