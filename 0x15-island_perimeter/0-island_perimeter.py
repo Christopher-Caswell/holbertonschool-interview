cbe54cf88ca8fbff34b6e544fb78217e9ba770ff
Christopher-Caswell <christopher.caswell@rocketmail.com>
2022-05-12 11:52:34 -0500

    More Pep8

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -13,7 +13,8 @@ def island_perimeter(grid):
     grid is rectangular, with its width and height not exceeding 100
     The grid is completely surrounded by water
     There is only one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+    The island doesn’t have “lakes”
+    (water inside that isn’t connected to the water surrounding the island).
     """
 
     dirt = 0
@@ -22,8 +23,8 @@ def island_perimeter(grid):
         for cols in range(len(grid[0])):
             if grid[rows][cols] == 1:
                 if rows == 0 or grid[rows - 1][cols] == 0:
-                   dirt += 1
-                if rows == len(grid) - 1 or grid[rows + 1 ][cols] == 0:
+                    dirt += 1
+                if rows == len(grid) - 1 or grid[rows + 1][cols] == 0:
                     dirt += 1
                 if cols == 0 or grid[rows][cols - 1] == 0:
                     dirt += 1
