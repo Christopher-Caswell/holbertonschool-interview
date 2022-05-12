1e84b58c7239f6caf32364071a8db7993554fa75
Christopher-Caswell <christopher.caswell@rocketmail.com>
2022-05-12 11:50:31 -0500

    Pep8ification

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -3,7 +3,7 @@
 
 
 def island_perimeter(grid):
-  """
+    """
     Given a grid of 1s and 0s, find the perimeter of the island.
     grid is a list of list of integers:
     0 represents water
@@ -14,20 +14,19 @@ def island_perimeter(grid):
     The grid is completely surrounded by water
     There is only one island (or nothing).
     The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
-  """
+    """
 
     dirt = 0
 
     for rows in range(len(grid)):
         for cols in range(len(grid[0])):
             if grid[rows][cols] == 1:
-            if rows == 0 or grid[rows - 1][cols] == 0:
-                dirt += 1
-            if rows == len(grid) - 1 or grid[rows + 1 ][cols] == 0:
-                dirt += 1
-            if cols == 0 or grid[rows][cols - 1] == 0:
-                dirt += 1
-            if cols == len(grid[0]) - 1 or grid[rows][cols + 1] == 0:
-                dirt += 1
-
+                if rows == 0 or grid[rows - 1][cols] == 0:
+                   dirt += 1
+                if rows == len(grid) - 1 or grid[rows + 1 ][cols] == 0:
+                    dirt += 1
+                if cols == 0 or grid[rows][cols - 1] == 0:
+                    dirt += 1
+                if cols == len(grid[0]) - 1 or grid[rows][cols + 1] == 0:
+                    dirt += 1
     return dirt
