afd506bc3945c01b2f36ea3d6ae792db38990de1
Christopher-Caswell <christopher.caswell@rocketmail.com>
2022-05-18 01:38:02 -0500

    pep8

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -26,10 +26,9 @@ def isWinner(x, nums):
     You cannot import any packages in this task
     """
 
-
     if not nums:
         return None
-    
+
     maria = 0
     ben = 0
     for i in range(x):
@@ -54,20 +53,27 @@ def isWinner(x, nums):
     else:
         return None
 
+
 def isPrime(n):
-    if n == 2 or n == 3: return True
-    if n < 2 or n%2 == 0: return False
-    if n < 9: return True
-    if n%3 == 0: return False
+    if n == 2 or n == 3:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
     r = int(n**0.5)
     # since all primes > 3 are of the form 6n ± 1
     # start with f=5 (which is prime)
     # and test f, f+2 for being prime
-    # then loop by 6. 
+    # then loop by 6.
     f = 5
     while f <= r:
         # print('\t',f)
-        if n % f == 0: return False
-        if n % (f+2) == 0: return False
+        if n % f == 0:
+            return False
+        if n % (f+2) == 0:
+            return False
         f += 6
-    return True    
+    return True
